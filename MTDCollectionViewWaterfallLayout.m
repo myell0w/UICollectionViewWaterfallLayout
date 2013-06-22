43243d4d971d4567f31e5f9f6cdbe058784bc2df
Matthias Tretter <matthias.tretter@gmail.com>
2013-06-22 23:57:19 +0200

    Add footerView to layout

--- MTDCollectionViewWaterfallLayout.m
+++ MTDCollectionViewWaterfallLayout.m
@@ -1,18 +1,16 @@
-//
-//  UICollectionViewWaterfallLayout.m
-//
-//  Created by Nelson on 12/11/19.
-//  Copyright (c) 2012 Nelson Tai. All rights reserved.
-//
-
 #import "MTDCollectionViewWaterfallLayout.h"
+
+
+#define kMTDFooterHeight 50.f
+
 
 @interface MTDCollectionViewWaterfallLayout()
 
 @property (nonatomic, assign) NSInteger itemCount;
 @property (nonatomic, assign) CGFloat interitemSpacing;
 @property (nonatomic, strong) NSMutableArray *columnHeights; // height for each column
-@property (nonatomic, strong) NSMutableArray *itemAttributes; // attributes for each item
+@property (nonatomic, strong) NSMutableDictionary *itemAttributes; // attributes for each item
+@property (nonatomic, strong) UICollectionViewLayoutAttributes *footerAttributes;
 @property (nonatomic, strong) NSMutableDictionary *columnIndexes;
 
 @property (nonatomic, strong) NSSet *indexPathsToInsert;
@@ -65,57 +63,9 @@
     return self;
 }
 
-#pragma mark - Methods to Override
+#pragma mark - UICollectionViewLayout
 
-- (void)invalidateLayout {
-    [super invalidateLayout];
-
-    [self.columnIndexes removeAllObjects];
-}
-
-- (void)prepareLayout
-{
-    [super prepareLayout];
-
-    if ([[self collectionView] numberOfSections] > 0) {
-        _itemCount = [[self collectionView] numberOfItemsInSection:0];
-
-        NSAssert(_columnCount > 1, @"columnCount for UICollectionViewWaterfallLayout should be greater than 1.");
-        CGFloat width = self.collectionView.frame.size.width - _sectionInset.left - _sectionInset.right;
-        _interitemSpacing = floorf((width - _columnCount * _itemWidth) / (_columnCount - 1));
-
-        _itemAttributes = [NSMutableArray arrayWithCapacity:_itemCount];
-        _columnHeights = [NSMutableArray arrayWithCapacity:_columnCount];
-        for (NSInteger idx = 0; idx < _columnCount; idx++) {
-            [_columnHeights addObject:@(_sectionInset.top)];
-        }
-
-        // Item will be put into shortest column.on first appear, afterwards they keep their column to not jump around
-        for (NSInteger idx = 0; idx < _itemCount; idx++) {
-            NSIndexPath *indexPath = [NSIndexPath indexPathForItem:idx inSection:0];
-            CGFloat itemHeight = [self.delegate collectionView:self.collectionView
-                                                        layout:self
-                                      heightForItemAtIndexPath:indexPath];
-
-            NSNumber *suggestedColumn = self.columnIndexes[indexPath];
-
-            NSUInteger columnIndex = suggestedColumn != nil ? [suggestedColumn unsignedIntegerValue] : [self shortestColumnIndex];
-            CGFloat xOffset = _sectionInset.left + (_itemWidth + _interitemSpacing) * columnIndex;
-            CGFloat yOffset = [(_columnHeights[columnIndex]) floatValue];
-
-            self.columnIndexes[indexPath] = @(columnIndex);
-
-            UICollectionViewLayoutAttributes *attributes = [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:indexPath];
-            attributes.frame = CGRectMake(xOffset, yOffset, self.itemWidth, itemHeight);
-            attributes.transform3D = CATransform3DIdentity;
-            [_itemAttributes addObject:attributes];
-            _columnHeights[columnIndex] = @(yOffset + itemHeight + _interitemSpacing);
-        }
-    }
-}
-
-- (CGSize)collectionViewContentSize
-{
+- (CGSize)collectionViewContentSize {
     if (self.itemCount == 0) {
         return CGSizeZero;
     }
@@ -123,51 +73,19 @@
     CGSize contentSize = self.collectionView.frame.size;
     NSUInteger columnIndex = [self longestColumnIndex];
     CGFloat height = [self.columnHeights[columnIndex] floatValue];
-    contentSize.height = height - self.interitemSpacing + self.sectionInset.bottom;
+    contentSize.height = height - self.interitemSpacing + 2*self.sectionInset.bottom + kMTDFooterHeight;
+
     return contentSize;
 }
 
-- (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)path
-{
-    return (self.itemAttributes)[path.item];
+- (void)invalidateLayout {
+    [super invalidateLayout];
+
+    [self.columnIndexes removeAllObjects];
 }
 
-- (UICollectionViewLayoutAttributes *)initialLayoutAttributesForAppearingItemAtIndexPath:(NSIndexPath *)itemIndexPath {
-    if ([self.indexPathsToInsert containsObject:itemIndexPath]) {
-        return [self layoutAttributesForAppearingOrDisappearingItemAtIndexPath:itemIndexPath];
-    } else {
-        return [self layoutAttributesForItemAtIndexPath:itemIndexPath];
-    }
-}
-
-//- (UICollectionViewLayoutAttributes *)finalLayoutAttributesForDisappearingItemAtIndexPath:(NSIndexPath *)itemIndexPath {
-//    if ([self.indexPathsToDelete containsObject:itemIndexPath]) {
-//        return [self layoutAttributesForAppearingOrDisappearingItemAtIndexPath:itemIndexPath];
-//    } else {
-//        return [self layoutAttributesForItemAtIndexPath:itemIndexPath];
-//    }
-//}
-
-// Private method: we want the same animation when inserting/deleting, just reversed
-- (UICollectionViewLayoutAttributes *)layoutAttributesForAppearingOrDisappearingItemAtIndexPath:(NSIndexPath *)itemIndexPath {
-    UICollectionViewLayoutAttributes *attributes = [self layoutAttributesForItemAtIndexPath:itemIndexPath];
-
-    attributes.transform3D = CATransform3DMakeScale(1/100.f, 1/100.f, 1.f);
-    attributes.alpha = 0.f;
-
-    return attributes;
-}
-
-- (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect
-{
-    return [self.itemAttributes filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(UICollectionViewLayoutAttributes *evaluatedObject, NSDictionary *bindings) {
-        return CGRectIntersectsRect(rect, [evaluatedObject frame]);
-    }]];
-}
-
-- (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds
-{
-    return NO;
+- (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds {
+    return YES;
 }
 
 // we keep track of the indexPaths of the objects that are actually appearing/disappearing
@@ -199,10 +117,122 @@
     self.indexPathsToDelete = nil;
 }
 
-#pragma mark - Private Methods
-// Find out shortest column.
-- (NSUInteger)shortestColumnIndex
-{
+
+- (void)prepareLayout {
+    if ([[self collectionView] numberOfSections] > 0) {
+        _itemCount = [[self collectionView] numberOfItemsInSection:0];
+
+        NSAssert(_columnCount > 1, @"columnCount for UICollectionViewWaterfallLayout should be greater than 1.");
+        CGFloat width = self.collectionView.frame.size.width - _sectionInset.left - _sectionInset.right;
+        _interitemSpacing = floorf((width - _columnCount * _itemWidth) / (_columnCount - 1));
+
+        _itemAttributes = [NSMutableDictionary dictionaryWithCapacity:_itemCount];
+        _columnHeights = [NSMutableArray arrayWithCapacity:_columnCount];
+        for (NSInteger idx = 0; idx < _columnCount; idx++) {
+            [_columnHeights addObject:@(_sectionInset.top)];
+        }
+
+        // Item will be put into shortest column.on first appear, afterwards they keep their column to not jump around
+        for (NSInteger idx = 0; idx < _itemCount; idx++) {
+            NSIndexPath *indexPath = [NSIndexPath indexPathForItem:idx inSection:0];
+            CGFloat itemHeight = [self.delegate collectionView:self.collectionView
+                                                        layout:self
+                                      heightForItemAtIndexPath:indexPath];
+
+            NSNumber *suggestedColumn = self.columnIndexes[indexPath];
+
+            NSUInteger columnIndex = suggestedColumn != nil ? [suggestedColumn unsignedIntegerValue] : [self shortestColumnIndex];
+            CGFloat xOffset = _sectionInset.left + (_itemWidth + _interitemSpacing) * columnIndex;
+            CGFloat yOffset = [(_columnHeights[columnIndex]) floatValue];
+
+            self.columnIndexes[indexPath] = @(columnIndex);
+
+            UICollectionViewLayoutAttributes *attributes = [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:indexPath];
+            attributes.frame = CGRectMake(xOffset, yOffset, self.itemWidth, itemHeight);
+            attributes.transform3D = CATransform3DIdentity;
+
+            _itemAttributes[indexPath] = attributes;
+            _columnHeights[columnIndex] = @(yOffset + itemHeight + _interitemSpacing);
+
+            // footer attributes
+            if (idx == _itemCount - 1) {
+                self.footerAttributes = [self layoutAttributesForSupplementaryViewOfKind:UICollectionElementKindSectionFooter
+                                                                             atIndexPath:indexPath];
+            }
+        }
+    }
+}
+
+- (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect {
+    NSMutableArray *layoutAttributes = [NSMutableArray arrayWithCapacity:self.itemAttributes.count+1];
+
+    [self.itemAttributes enumerateKeysAndObjectsUsingBlock:^(NSIndexPath *indexPath, MTDCollectionViewLayoutAttributes *attributes, BOOL *stop) {
+        if (CGRectIntersectsRect(rect, attributes.frame)) {
+            [layoutAttributes addObject:attributes];
+        }
+    }];
+
+    if (CGRectIntersectsRect(rect, self.footerAttributes.frame)) {
+        [layoutAttributes addObjectIfNotNil:self.footerAttributes];
+    }
+
+    return layoutAttributes;
+}
+
+- (UICollectionViewLayoutAttributes *)layoutAttributesForSupplementaryViewOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewLayoutAttributes *attributes = [super layoutAttributesForSupplementaryViewOfKind:kind atIndexPath:indexPath];
+
+    if ([kind isEqualToString:UICollectionElementKindSectionFooter]) {
+        CGSize contentSize = self.collectionViewContentSize;
+
+        if (attributes == nil) {
+            attributes = [UICollectionViewLayoutAttributes layoutAttributesForSupplementaryViewOfKind:kind withIndexPath:indexPath];
+        }
+        
+        attributes.frame = CGRectMake(self.sectionInset.left,
+                                      contentSize.height - kMTDFooterHeight - self.sectionInset.bottom,
+                                      contentSize.width,
+                                      kMTDFooterHeight);
+    }
+
+    return attributes;
+}
+
+- (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)path {
+    return self.itemAttributes[path];
+}
+
+//- (UICollectionViewLayoutAttributes *)initialLayoutAttributesForAppearingItemAtIndexPath:(NSIndexPath *)itemIndexPath {
+//    if ([self.indexPathsToInsert containsObject:itemIndexPath]) {
+//        return [self layoutAttributesForAppearingOrDisappearingItemAtIndexPath:itemIndexPath];
+//    } else {
+//        return [self layoutAttributesForItemAtIndexPath:itemIndexPath];
+//    }
+//}
+
+//- (UICollectionViewLayoutAttributes *)finalLayoutAttributesForDisappearingItemAtIndexPath:(NSIndexPath *)itemIndexPath {
+//    if ([self.indexPathsToDelete containsObject:itemIndexPath]) {
+//        return [self layoutAttributesForAppearingOrDisappearingItemAtIndexPath:itemIndexPath];
+//    } else {
+//        return [self layoutAttributesForItemAtIndexPath:itemIndexPath];
+//    }
+//}
+
+// Private method: we want the same animation when inserting/deleting, just reversed
+- (UICollectionViewLayoutAttributes *)layoutAttributesForAppearingOrDisappearingItemAtIndexPath:(NSIndexPath *)itemIndexPath {
+    UICollectionViewLayoutAttributes *attributes = [self layoutAttributesForItemAtIndexPath:itemIndexPath];
+
+    attributes.transform3D = CATransform3DMakeScale(1/100.f, 1/100.f, 1.f);
+    attributes.alpha = 0.f;
+
+    return attributes;
+}
+
+////////////////////////////////////////////////////////////////////////
+#pragma mark - Private
+////////////////////////////////////////////////////////////////////////
+
+- (NSUInteger)shortestColumnIndex {
     __block NSUInteger index = 0;
     __block CGFloat shortestHeight = MAXFLOAT;
 
@@ -217,9 +247,7 @@
     return index;
 }
 
-// Find out longest column.
-- (NSUInteger)longestColumnIndex
-{
+- (NSUInteger)longestColumnIndex {
     __block NSUInteger index = 0;
     __block CGFloat longestHeight = 0;
 
